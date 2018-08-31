dcf0b145bf2e71cf43c30ae51ddb47a883eb84bd
Florian Klampfer <mail@qwtel.com>
2018-08-31 19:08:04 +0800

    update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,14 @@
-const { resolve } = require('path');
+const { resolve } = require("path");
 
+const { BannerPlugin, EnvironmentPlugin } = require("webpack");
+
+const merge = require("webpack-merge");
 const {
-  BannerPlugin,
-  EnvironmentPlugin,
-  optimize: {
-    UglifyJsPlugin,
-    ModuleConcatenationPlugin,
-  },
-} = require('webpack');
+  argv: { mode }
+} = require("yargs");
+const camelcase = require("camelcase");
 
-const merge = require('webpack-merge');
-const { argv: { env } } = require('yargs');
-const camelcase = require('camelcase');
-
-const { name: filename } = require('./package.json');
+const { name: filename } = require("./package.json");
 
 const banner = String.prototype.trim.call(`
 Copyright (c) 2018 Florian Klampfer <https://qwtel.com/>
@@ -22,60 +17,52 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 `);
 const library = camelcase(filename);
-const min = env === 'prod' ? '.min' : '';
+const min = mode === "production" ? ".min" : "";
 
-function envConfig() {
-  switch (env) {
-    case 'prod':
-      return {
+const envConfig =
+  mode === "production"
+    ? {
         plugins: [
           new BannerPlugin({ banner }),
-          new EnvironmentPlugin({ DEBUG: false }),
-          new UglifyJsPlugin(),
-        ],
+          new EnvironmentPlugin({ DEBUG: false })
+        ]
+      }
+    : {
+        devtool: "source-map",
+        plugins: [new EnvironmentPlugin({ DEBUG: true })]
       };
 
-    default:
-      return {
-        devtool: 'source-map',
-        plugins: [
-          new EnvironmentPlugin({ DEBUG: true }),
-        ],
-      };
-  }
-}
-
-const baseConfig = merge({
-  output: {
-    path: resolve('./dist'),
-    library,
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
+const baseConfig = merge(
+  {
+    output: {
+      path: resolve("./dist"),
+      library,
+      libraryTarget: "umd",
+      umdNamedDefine: true
+    },
+    module: {
+      rules: [
+        {
+          test: /(\.jsx|\.js)$/,
+          loader: "babel-loader"
+        }
+      ]
+    },
+    resolve: {
+      modules: [resolve("./node_modules")],
+      extensions: [".json", ".js"]
+    }
   },
-  module: {
-    rules: [
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'babel-loader',
-      },
-    ],
-  },
-  resolve: {
-    modules: [resolve('./node_modules')],
-    extensions: ['.json', '.js'],
-  },
-  plugins: [
-    new ModuleConcatenationPlugin(),
-  ],
-}, envConfig());
+  envConfig
+);
 
 const config = [
   merge(baseConfig, {
-    entry: resolve('./index.js'),
+    entry: resolve("./index.js"),
     output: {
-      filename: `index${min}.js`,
-    },
-  }),
+      filename: `index${min}.js`
+    }
+  })
 ];
 
 module.exports = config;
